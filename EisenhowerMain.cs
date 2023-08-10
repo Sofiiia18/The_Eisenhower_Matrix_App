e19b819f919e71a78031a73cf35c7de376662c91
Sofiiia18 <122811257+Sofiiia18@users.noreply.github.com>
2023-08-10 11:04:15 +0300

    Class TodoItem, Class TodoQuarter almost done

---
+++ EisenhowerMain.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EisenhowerCore
+{
+    public class EisenhowerMain
+    {  
+        static public void Main(String[] args) 
+        { 
+            Console.WriteLine("Hello Codecool!"); 
+        }
+    }
+}
